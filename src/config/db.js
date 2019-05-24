fe48c3aeffc80f886ffaecc30a9d80584a767480
Gabriel Pasquale <pasqualeunq@gmail.com>
2019-05-23 21:16:04 -0300

    db test

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,11 @@
 const mongoose = require('mongoose');
-const { Mockgoose } = require('mockgoose');
 
-const DB_URI = 'mongodb://localhost:27017/tp';
+const DB_URI = 'mongodb://localhost/tp';
 
-const connect = () => {
-  if (process.env.NODE_ENV === 'test') {
-    const mockgoose = new Mockgoose(mongoose);
-    return mockgoose.prepareStorage().then(() => {
-      return mongoose.connect(DB_URI, { useNewUrlParser: true });
-    });
-  } else {
-    return mongoose.connect(DB_URI, { useNewUrlParser: true });
-  }
-};
+const DB_URI_TEST = 'mongodb://localhost/tp_test';
+
+const connect = () =>
+  mongoose.connect(process.env.NODE_ENV === 'test' ? DB_URI_TEST : DB_URI, { useNewUrlParser: true });
 
 const close = () => {
   return mongoose.disconnect();
